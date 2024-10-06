0bcd1cef9dff57ef9845559bfe1722565fd71f7d
its-Rupesh <rupeshchimankare2004@gmail.com>
2024-10-06 20:19:11 +0530

    Group Chat Manage Page

--- client/src/pages/Group.jsx
+++ client/src/pages/Group.jsx
@@ -1,7 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  KeyboardBackspace as KeyboardBackspaceIcon,
+  Menu as MenuIcon,
+} from "@mui/icons-material";
+import { Box, Drawer, Grid, IconButton, Tooltip } from "@mui/material";
+import { matBlack } from "../constants/color";
+import { useNavigate } from "react-router-dom";
 
 const Group = () => {
-  return <div>Group</div>;
+  const navigate = useNavigate();
+  const navigateBack = () => {
+    navigate("/");
+  };
+  const [isMobileMenuOpen, setisMobileMenuOpen] = useState(false);
+  const handleMobile = () => {
+    setisMobileMenuOpen((prev) => !prev);
+  };
+  const handleMobileClose = () => {
+    setisMobileMenuOpen(false);
+  };
+  const IconBtns = (
+    <>
+      <Box
+        sx={{
+          display: {
+            xs: "block",
+            sm: "none",
+            position: "fixed",
+            right: "1rem",
+            top: "1rem",
+          },
+        }}
+      >
+        <Tooltip title="Menu">
+          <IconButton onClick={handleMobile}>
+            <MenuIcon />
+          </IconButton>
+        </Tooltip>
+      </Box>
+      <Tooltip title="Back">
+        <IconButton
+          sx={{
+            position: "absolute",
+            top: "2rem",
+            left: "2rem",
+            bgcolor: matBlack,
+            color: "white",
+            ":hover": {
+              bgcolor: "rgba(0,0,0,0.7)",
+            },
+          }}
+          onClick={navigateBack}
+        >
+          <KeyboardBackspaceIcon />
+        </IconButton>
+      </Tooltip>
+    </>
+  );
+
+  return (
+    <Grid container height={"100vh"}>
+      <Grid
+        item
+        sx={{ display: { xs: "none", sm: "block" } }}
+        sm={4}
+        bgcolor={"bisque"}
+      >
+        Groups List
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        sm={8}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          position: "relative",
+          padding: "1rem 3rem",
+        }}
+      >
+        {IconBtns}
+      </Grid>
+      <Drawer
+        sx={{ display: { xs: "block", sm: "none" } }}
+        open={isMobileMenuOpen}
+        onClose={handleMobileClose}
+      >
+        Group List
+      </Drawer>
+    </Grid>
+  );
 };
+
+
 
 export default Group;
