db29948ccb863b7a8883a737e30d7c803de608b0
its-Rupesh <rupeshchimankare2004@gmail.com>
2024-10-13 23:19:23 +0530

    Break

--- client/src/components/styles/styledComponent.jsx
+++ client/src/components/styles/styledComponent.jsx
@@ -40,15 +40,15 @@ export const SearchField = styled("input")`
   font-size: 1.1rem;
 `;
 
-export const CurveButton = styled("button")`
-  border-radius: 1.5rem;
-  padding: 1rem 2rem;
-  border: none;
-  outline: none;
-  cursor: pointer;
-  background-color: ${matBlack};
-  color: white;
-  font-size: 1.1rem;
+// export const CurveButton = styled("button")`
+//   border-radius: 1.5rem;
+//   padding: 1rem 2rem;
+//   border: none;
+//   outline: none;
+//   cursor: pointer;
+//   background-color: ${matBlack};
+//   color: white;
+//   font-size: 1.1rem;
 
   &:hover {
     background-color: rgba(0, 0, 0, 0.8);
