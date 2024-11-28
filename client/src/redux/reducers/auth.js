0ec5b0d38fdb23b3eee279225dc9faca16a68a90
its-Rupesh <rupeshchimankare2004@gmail.com>
2024-11-28 10:40:01 +0530

    Slice Created 2 Actions ->userExist and userNotExist

---
+++ client/src/redux/reducers/auth.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+initialState = {
+  user: null,
+  isAdmin: false,
+  loader: true,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    userExist: (state, action) => {
+      state.user = action.payload;
+      state.loader = false;
+    },
+    userNotExist: (state) => {
+      state.user = null;
+      state.loader = false;
+    },
+  },
+});
+export const { userExist, userNotExist } = authSlice.actions;
+export default authSlice;
