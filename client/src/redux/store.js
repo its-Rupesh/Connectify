9bd0b4602a0a60c295a07e2fce3d89ff4489f4f5
its-Rupesh <rupeshchimankare2004@gmail.com>
2024-11-28 10:54:50 +0530

    Auth slice Register in store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,3 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-const store = configureStore({ reducer: {} });
+import authSlice from "./reducers/auth";
+const store = configureStore({
+  reducer: {
+    reducer: {
+      //  Reducer name
+      [authSlice.name]: authSlice.reducer,
+    },
+  },
+});
 export default store;
